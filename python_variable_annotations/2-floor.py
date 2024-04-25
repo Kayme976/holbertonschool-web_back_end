15c80db7236d152d4431f04944f757552d0a9714
Kayme976 <7508@holbertonstudents.com>
2024-04-25 23:15:06 +0200

    Fix typos and formatting in Python variable annotations

--- python_variable_annotations/2-floor.py
+++ python_variable_annotations/2-floor.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-""" annotation-floor"""
+"""annotation floor"""
 import math
 
 
--- python_variable_annotations/3-to_str.py
+++ python_variable_annotations/3-to_str.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """annotation to strings"""
 
+
 def to_str(n: float) -> str:
     """Return the string representation of the given float."""
     return str(n)
