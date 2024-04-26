a6f3da1b0fa7483a0f3b7cafc969fcedfc386eae
Kayme976 <7508@holbertonstudents.com>
2024-04-26 22:40:08 +0200

    Update docstring in measure_runtime function in 2-measure_runtime.py

--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -7,7 +7,7 @@ async_comprehension = __import__('1-async_comprehension').async_comprehension
 
 
 async def measure_runtime() -> float:
-    """will execute async_comprehension four times in parallel using asyncio.gather."""
+    """will execute async_comprehension four times"""
     begin_time = time.time()
     rdm = [async_comprehension() for i in range(4)]
     result = await asyncio.gather(*rdm)
