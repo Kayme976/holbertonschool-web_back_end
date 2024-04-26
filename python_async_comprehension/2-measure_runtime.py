bf04755b275c360e8a83d8c1f35bf10c0bfa3f22
Kayme976 <7508@holbertonstudents.com>
2024-04-26 22:35:07 +0200

    Add async generator and async comprehension to Python async files

---
+++ python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""Import async_comprehension from the previous file """
+import asyncio
+import time
+
+async_comprehension = __import__('1-async_comprehension').async_comprehension
+
+
+async def measure_runtime() -> float:
+    """will execute async_comprehension four times in parallel using asyncio.gather."""
+    begin_time = time.time()
+    rdm = [async_comprehension() for i in range(4)]
+    result = await asyncio.gather(*rdm)
+    end_time = time.time()
+    return end_time - begin_time
